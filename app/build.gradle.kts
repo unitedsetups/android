4b08f7ff8cfb5812a3e69d215ea0b6d94246825a
Paras Khanchandani <dev@paraskcd.com>
2024-12-31 01:56:49 +0100

    updated version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.paraskcd.unitedsetups"
         minSdk = 30
         targetSdk = 34
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
